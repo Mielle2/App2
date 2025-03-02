fead0205000cf30fa0ec5eb124e9132e561467f7
Mielle <tuyetnhux@gmail.com>
2025-03-02 21:39:43 +0100

    added manifest and sw

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
-MM207
+Uke 3
+Oppgave: Poem, Quote, sum
 
 Uke 4
+Oppgave: Kortstokk
 Playing cards pngs was downloaded from https://github.com/hanhaechi/playing-cards
 
-Render:
-https://mielle-2dr7.onrender.com 
+Uke 5
+Oppgave: Middelware
+
+Uke 8
+PROD (render): https://mielle-2dr7.onrender.com 
+
+Uke 9
+Oppgave: 
+PWA
+
+---------------------------------------------------------------------------------------
+Er ikke ferdig enda og mangler flere justeringer for å optimalisere den. Trenger å rydde opp i filer.
--- server.mjs
+++ server.mjs
@@ -1,16 +1,48 @@
 import express from "express";
 import HTTP_CODES from "./utils/httpCodes.mjs";
-import userRouter from "./routes/userAPI.mjs";
+import log from './modules/log.mjs';
+import { LOGG_LEVELS, eventLogger } from './modules/log.mjs';
+import { startSession, updateSession } from './modules/sessions.mjs';
+import treeRouter from './routes/treeAPI.mjs';
+import questLogRouter from './routes/questLogAPI.mjs';
+import userRouter from './routes/userAPI.mjs';
+
+import { getRoot } from './modules/uke2.mjs';
+import { getPoem, getQuote, getSum } from './modules/uke3.mjs';
+import { makeDeck, shuffleDeck, getDeck, getCard } from './modules/uke4.mjs';
+
+const ENABLE_LOGGING = false;
 
 const server = express();
 const port = process.env.PORT || 8000;
 
+const logger = log(LOGG_LEVELS.VERBOSE);
+setSessionLogLevel('verbose');
+
 server.set("port", port);
 
+server.use(logger);
 server.use(express.json());
 server.use(express.static("public"));
+server.use(startSession);
+server.use("/tree/", treeRouter);
+server.use("/quest", questLogRouter);
+server.use("/user", userRouter)
+server.use(updateSession);
 
-server.use("/user", userRouter);
+// Uke 2
+server.get("/", getRoot);
+
+// Uke 3
+server.get("/tmp/poem", getPoem);
+server.get("/tmp/quote", getQuote);
+server.post("/tmp/sum/:a/:b", getSum);
+
+// Uke 4
+server.post("/temp/deck", makeDeck);
+server.patch("/temp/deck/shuffle/:deck_id", shuffleDeck);
+server.get("/temp/deck/:deck_id", getDeck);
+server.get("/temp/deck/:deck_id/card", getCard);
 
 server.listen(server.get("port"), function () {
   console.log("server running", server.get("port"));
