1eb180423f713cdc4a7972cd2bf2654508ef786d
Mielle <tuyetnhux@gmail.com>
2025-03-13 22:17:06 +0100

    adjustments in folders

--- data/ingredient.mjs
+++
@@ -1,9 +0,0 @@
-class Ingredient {
-    constructor(name, compatibility, description) {
-        this.name = name;
-        this.compatibility = compatibility;
-        this.description = description;
-    }
-}
-
-export default Ingredient;
