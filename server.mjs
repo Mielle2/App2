095af404770aaf2814501fecc907a662f464c598
Mielle <tuyetnhux@gmail.com>
2025-03-15 16:44:28 +0100

    commented out session

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import sequelize from "./data/db.js";
 import ingredientAPI from "./routes/ingredientAPI.mjs";
-import { startSession, updateSession } from "./middleware/session.mjs";
+//import { startSession, updateSession } from "./middleware/session.mjs";
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -10,13 +10,13 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.use(startSession);
+//app.use(startSession);
 
 app.use("/api", ingredientAPI);
 
 app.get("/", (req, res, next) => res.send("Backend API kjører!"));
 
-app.use(updateSession);
+//app.use(updateSession);
 
 sequelize.sync().then(() => {
     app.listen(port, () => {
